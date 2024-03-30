8e9ad35c7044c02c46abf5122ea0d6e5ed2c836e
rvanetta97 <rachel.vanetta@gmail.com>
2024-03-30 13:30:55 -0400

    results page documentation

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const resultEl = document.querySelector('#result-text');
+const resultContentEl = document.querySelector('#result-content');
+const searchEl = document.querySelector('#search-form');
